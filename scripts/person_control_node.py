3f4739862058905f103bd6c13d44048dcabd146e
Ryan Fainor <fainorr@lafayette.edu>
2020-04-26 22:41:07 -0400

    Update person_control_node.py

--- scripts/person_control_node.py
+++ scripts/person_control_node.py
@@ -20,8 +20,8 @@ class person_drive():
 		self.timenow = time.time()
 		self.oldtime = self.timenow
 
-    	self.velocity_person1 = rospy.Publisher('/vel_person1', Twist, queue_size=1)
-    	self.velocity_person2 = rospy.Publisher('/vel_person2', Twist, queue_size=1)
+		self.velocity_person1 = rospy.Publisher('/vel_person1', Twist, queue_size=1)
+		self.velocity_person2 = rospy.Publisher('/vel_person2', Twist, queue_size=1)
 		self.velocity_person3 = rospy.Publisher('/vel_person3', Twist, queue_size=1)
 
 		# create loop
@@ -30,46 +30,46 @@ class person_drive():
 	def loop(self, event):
 
 		self.v1 = Twist()
-        self.v2 = Twist()
-        self.v3 = Twist()
+		self.v2 = Twist()
+		self.v3 = Twist()
 
-        velocity = random.normal(1.4,0.2)
-        angle_deg = random.randint(1,360)
-        angle = float(angle_deg)*pi/180
+		velocity = random.normal(1.4,0.2)
+		angle_deg = random.randint(1,360)
+		angle = float(angle_deg)*pi/180
 
-        self.v1.linear.x = velocity*cos(angle);
-        self.v1.linear.y = velocity*sin(angle);
-        self.v1.linear.z = 0.0;
-        self.v1.angular.x = 0.0;
-        self.v1.angular.y = 0.0;
-        self.v1.angular.z = 0.0;
+		self.v1.linear.x = velocity*cos(angle);
+		self.v1.linear.y = velocity*sin(angle);
+		self.v1.linear.z = 0.0;
+		self.v1.angular.x = 0.0;
+		self.v1.angular.y = 0.0;
+		self.v1.angular.z = 0.0;
 
-        velocity = random.normal(1.4,0.2)
-        angle_deg = random.randint(1,360)
-        angle = float(angle_deg)*pi/180
+		velocity = random.normal(1.4,0.2)
+		angle_deg = random.randint(1,360)
+		angle = float(angle_deg)*pi/180
 
-        self.v2.linear.x = velocity*cos(angle);
-        self.v2.linear.y = velocity*sin(angle);
-        self.v2.linear.z = 0.0;
-        self.v2.angular.x = 0.0;
-        self.v2.angular.y = 0.0;
-        self.v2.angular.z = 0.0;
+		self.v2.linear.x = velocity*cos(angle);
+		self.v2.linear.y = velocity*sin(angle);
+		self.v2.linear.z = 0.0;
+		self.v2.angular.x = 0.0;
+		self.v2.angular.y = 0.0;
+		self.v2.angular.z = 0.0;
 
-        velocity = random.normal(1.4,0.2)
-        angle_deg = random.randint(1,360)
-        angle = float(angle_deg)*pi/180
+		velocity = random.normal(1.4,0.2)
+		angle_deg = random.randint(1,360)
+		angle = float(angle_deg)*pi/180
 
-        self.v3.linear.x = velocity*cos(angle);
-        self.v3.linear.y = velocity*sin(angle);
-        self.v3.linear.z = 0.0;
-        self.v3.angular.x = 0.0;
-        self.v3.angular.y = 0.0;
-        self.v3.angular.z = 0.0;
+		self.v3.linear.x = velocity*cos(angle);
+		self.v3.linear.y = velocity*sin(angle);
+		self.v3.linear.z = 0.0;
+		self.v3.angular.x = 0.0;
+		self.v3.angular.y = 0.0;
+		self.v3.angular.z = 0.0;
 
 
 		self.velocity_person1.publish(self.v1)
-        self.velocity_person2.publish(self.v2)
-        self.velocity_person3.publish(self.v3)
+		self.velocity_person2.publish(self.v2)
+		self.velocity_person3.publish(self.v3)
 
 
 

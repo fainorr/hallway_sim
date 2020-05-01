9c89821210b3390ba5fd290600391fc01af230f2
Ryan Fainor <fainorr@lafayette.edu>
2020-04-30 23:18:30 -0400

    parse lidar analysis methods into functions

--- scripts/lidar_compare.py
+++ scripts/lidar_compare.py
@@ -3,6 +3,7 @@
 from numpy import *
 import rospy
 import roslib
+from quad_analysis_methods import *
 
 # -------------
 # LIDAR COMPARE
@@ -55,60 +56,15 @@ class lidar_compare():
 			if distances[i] > safe_range: in_range[i] = 0
 			else: in_range[i] = 1
 
+		# method = "QUADRANT"
+		quad_obstacles = analyze_quadrant(obst_size, in_range)
 
-		# METHOD = "QUADRANT"
+		# method = "PERCENT"
+		obst_percent = analyze_percent(in_range)
 
-		for quad in range(0,4):
-			quad_values = zeros((90-obst_size,1))
-
-			for i in range(90*quad, 90*(quad+1) - obst_size):
-				scan_obst_size = 0
-
-				for k in range(0,obst_size):
-					if in_range[i+k] == 1: scan_obst_size = scan_obst_size + 1
-
-				if scan_obst_size == obst_size: quad_values[i-90*quad] = 1
-
-			if sum(quad_values >= 1): quad_obstacles[quad] = 1
-
-
-		# METHOD = "PERCENT"
-
-		quad_points = [0.,0.,0.,0.]
-
-		for quad in range(0,4):
-			for i in range(90*quad, 90*(quad+1)):
-				if in_range[i] == 1: quad_points[quad] = quad_points[quad] + 1
-
-		if sum(quad_points) == 0:
-			obst_percent = 0.0
-		else:
-			obst_percent = quad_points/sum(quad_points)
-
-
-		# METHOD = "INTENSITY"
-
-		quad_points = [0.,0.,0.,0.]
-
-		for quad in range(0,4):
-			for i in range(90*quad, 90*(quad+1)):
-				if distances[i] != inf:
-					quad_points[quad] = quad_points[quad] + distances[i]**2
-
-		for quad in range(0,4):
-			if quad_points[quad] > 0:
-				obst_intensity[quad] = sum(quad_points)/quad_points[quad]
-			else:
-				obst_intensity[quad] = inf
-
-		total_obst_intensity = sum(obst_intensity)
-
-		for quad in range(0,4):
-			if total_obst_intensity == 0.0:
-				obst_intensity[quad] = 0.0
-			else:
-				obst_intensity[quad] = obst_intensity[quad]/total_obst_intensity
-
+		# method = "INTENSITY"
+		obst_intensity = analyze_intensity(distances)
+		
 
 		# FINDING ACTION AND DIRECTION
 

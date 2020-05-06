0b9a640bf92b2cee53b9ef584ec5031aa1682d6e
Ryan Fainor <fainorr@lafayette.edu>
2020-05-05 20:24:23 -0400

    Update eval_sim.py

--- eval/eval_sim.py
+++ eval/eval_sim.py
@@ -12,7 +12,7 @@ from matplotlib import _color_data
 
 # plot hallway links with robot path to evaluation navigation simulations
 
-timestamp = '20-04-30-192858'
+timestamp = '20-05-05-183508'
 
 
 # extract hallway data from text file
@@ -37,11 +37,12 @@ for y in range(0,len(hall_full)):
 
 # extract robot data from text file
 
-with open('eval/{}_hall.txt'.format(timestamp), 'r') as file:
+with open('eval/{}_robot.txt'.format(timestamp), 'r') as file:
 	robot_full = file.readlines()
 
 robot_xpos = []
 robot_ypos = []
+robot_collision = []
 
 for y in range(0,len(robot_full)):
 	row = robot_full[y].split(' ')
@@ -49,17 +50,19 @@ for y in range(0,len(robot_full)):
 	if y != 0:
 		robot_xpos.append(float(row[0]))
 		robot_ypos.append(float(row[1]))
+		robot_collision.append(row[2].strip('\n'))
 
 
-# prepare for plot
+# ---------------
+# plot simulation
+# ---------------
 
 # find axes max and min limits
 ax_max = max(max(link_xpos)+2, max(link_ypos)+2)
 ax_min = max(min(link_xpos)-2, min(link_ypos)-2)
 
 
-# plot in x, y plane
-
+# set up x,y plane for figure
 fig = plt.figure(figsize=(6,6))
 fig.patch.set_facecolor('w')
 ax = plt.axes(xlim=(ax_min, ax_max),ylim=(ax_min, ax_max))
@@ -74,15 +77,35 @@ for i in range(0,len(link_xpos)):
 	rect_len = link_xsize[i]
 	rect_height = link_ysize[i]
 
-	box = plt.Rectangle((rect_x, rect_y), rect_len, rect_height, fc='xkcd:medium blue')
+	box = plt.Rectangle((rect_x, rect_y), rect_len, rect_height, fc='xkcd:charcoal')
 	plt.gca().add_patch(box)
 
 
-# plot robot path
+# plot robot path, with collisions in a separate color
 
 for i in range(0,len(robot_xpos)):
 
-	scatter = ax.scatter(robot_xpos[i], robot_ypos[i], s=3, c='xkcd:apple green')
+	if robot_collision[i] == 'False':
+		scatter = ax.scatter(robot_xpos[i], robot_ypos[i], s=4, c='xkcd:cadet blue')
+	else:
+		scatter = ax.scatter(robot_xpos[i], robot_ypos[i], s=4, c='xkcd:neon blue')
 
+
+# add collision annotation
+
+bumper_text = 'Bumper Test: PASS'
+n_collisions = 0
+
+for i in range(0,len(robot_collision)):
+	if robot_collision[i] == 'True':
+		bumper_text = 'Bumper Test: FAIL'
+		n_collisions += 1
+
+plt.annotate(bumper_text, xy=(0, 0), xytext=(0.05, 0.95), textcoords='axes fraction',
+        horizontalalignment='left', verticalalignment='top', fontsize=9)
+
+collisions_text = 'Collisions: {0}/{1}'.format(n_collisions, len(robot_collision))
+plt.annotate(collisions_text, xy=(0, 0), xytext=(0.05, 0.9), textcoords='axes fraction',
+		horizontalalignment='left', verticalalignment='top', fontsize=9)
 
 plt.show()

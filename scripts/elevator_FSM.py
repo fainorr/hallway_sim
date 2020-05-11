1f104007c52a392d12fa88757e0534d4a1c1042b
jmconner419 <56118450+jmconner419@users.noreply.github.com>
2020-05-11 09:52:13 -0400

    Update elevator_FSM.py
    Updates to finite state machine, incorporating button presses into state transitions

--- scripts/elevator_FSM.py
+++ scripts/elevator_FSM.py
@@ -30,25 +30,19 @@ class Elevator_FSM():
 		rospy.Timer(rospy.Duration(self.dT), self.loop, oneshot=False)
 
 		self.Ready = 1
-		self.Open = 0
-		self.Close = 0
+		self.Enter = 0
 		self.Moving = 0
-		self.Reopen = 0
-		self.Reclose = 0
+		self.Exit = 0
 
 		self.T0_EN = 0
 		self.T0 = 0
 		self.T1_EN = 0
 		self.T1 = 0
-		self.T2_EN = 0
-		self.T2 = 0
 
 		self.wait_time0 = 1
 		self.timing_time0 = 0
 		self.wait_time1 = 1
 		self.timing_time1 = 0
-		self.wait_time2 = 1
-		self.timing_time2 = 0
 
 		self.A_time0 = 0
 		self.B_time0 = 0
@@ -58,17 +52,11 @@ class Elevator_FSM():
 		self.B_time1 = 0
 		self.C_time1 = 0
 		self.D_time1 = 0
-		self.A_time2 = 0
-		self.B_time2 = 0
-		self.C_time2 = 0
-		self.D_time2 = 0
 
 		self.delta_t0 = 0
 		self.Start_time0 = 0
 		self.delta_t1 = 0
 		self.Start_time1 = 0
-		self.delta_t2 = 0
-		self.Start_time2 = 0
 
 		self.outer_button_states = ContactState()
 		self.inner_button_states = ContactState()
@@ -81,18 +69,13 @@ class Elevator_FSM():
 		self.F = 0
 		self.G = 0
 		self.H = 0
-		self.I = 0
-		self.J = 0
-		self.K = 0
-		self.L = 0
 
 	def loop(self, event):
 
 		# Block 1
 
-		self.T0_EN = self.Open
-		self.T1_EN = self.Moving
-		self.T2_EN = self.Reopen
+		self.T0_EN = self.Moving
+		self.T1_EN = self.Exit
 
 		#--------------------TIMER_0-------------------------
 		self.A_time0 = self.wait_time0 and self.T0_EN
@@ -115,7 +98,7 @@ class Elevator_FSM():
 		self.T0 = self.delta_t0 > 5
 		#----------------------------------------------------
 
-		#--------------------TIMER_0-------------------------
+		#--------------------TIMER_1-------------------------
 		self.A_time1 = self.wait_time1 and self.T1_EN
 		self.B_time1 = self.wait_time1 and not self.T1_EN
 		self.C_time1 = self.timing_time1 and not self.T1_EN
@@ -133,28 +116,7 @@ class Elevator_FSM():
 		else:
 			self.delta_t1 = 0
 
-		self.T1 = self.delta_t1 > 5
-		#----------------------------------------------------
-
-		#--------------------TIMER_0-------------------------
-		self.A_time2 = self.wait_time2 and self.T2_EN
-		self.B_time2 = self.wait_time2 and not self.T2_EN
-		self.C_time2 = self.timing_time2 and not self.T2_EN
-		self.D_time2 = self.timing_time2 and self.T2_EN
-
-		self.wait_time2 = self.B_time2 or self.C_time2
-		self.timing_time2  =self.A_time2 or self.D_time2
-
-		if(self.A_time2):
-			self.Start_time2 = time.time()
-
-		if(self.timing_time2):
-			self.delta_t2 = time.time() - self.Start_time2
-
-		else:
-			self.delta_t2 = 0
-
-		self.T2 = self.delta_t2 > 5
+		self.T1 = self.delta_t1 > 15
 		#----------------------------------------------------
 
 		# button presses
@@ -173,32 +135,26 @@ class Elevator_FSM():
 
 		self.A = self.Ready and not self.outer_button_press
 		self.B = self.Ready and self.outer_button_press
-		self.C = self.Open and not self.T0
-		self.D = self.Open and self.T0
-		self.E = self.Close and not self.closed
-		self.F = self.Close and self.closed
-		self.G = self.Moving and not self.T1
-		self.H = self.Moving and self.T1
-		self.I = self.Reopen and not self.T2
-		self.J = self.Reopen and self.T2
-		self.K = self.Reclose and not self.closed
-		self.L = self.Reclose and self.closed
+		self.C = self.Enter and not self.inner_button_press
+		self.D = self.Enter and self.inner_button_press
+		self.E = self.Moving and not self.T0
+		self.F = self.Moving and self.T0
+		self.G = self.Exit and not self.T1
+		self.H = self.Exit and self.T1
 
 		# Block 3
 
-		self.Ready = self.A or self.L
-		self.Open = self.B or self.C
-		self.Close = self.D or self.E
-		self.Moving = self.F or self.G
-		self.Reopen = self.H or self.I
-		self.Reclose = self.J or self.K
+		self.Ready = self.A or self.H
+		self.Enter = self.B or self.C
+		self.Moving = self.D or self.E
+		self.Exit = self.F or self.G
 
 
 		# Block 4
-		if self.Open or self.Reopen:
+		if self.Enter or self.Exit:
 			action = 'open'
 
-		if self.Close or self.Reclose or self.Ready or self.Moving:
+		if self.Ready or self.Moving:
 			action = 'close'
 
 		self.door_action.publish(action)
